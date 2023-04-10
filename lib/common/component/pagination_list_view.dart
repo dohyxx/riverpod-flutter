d3490e4af34fa0bc8425b2bbf35b6db86ad9e4a6
Dohyun <dohyun1225@gmail.com>
2023-04-11 01:04:19 +0900

    RefreshIndicator 추가

--- lib/common/component/pagination_list_view.dart
+++ lib/common/component/pagination_list_view.dart
@@ -90,28 +90,36 @@ class _PaginationListViewState<T extends IModelWithId> extends ConsumerState<Pag
 
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
-      child: ListView.separated(
-        controller: controller,
-        itemCount: cp.data.length + 1,
-        itemBuilder: (_, index) {
-          if (index == cp.data.length) {
-            return Center(
-              child: cp is CursorPaginationfetchingMore
-                  ? const CircularProgressIndicator()
-                  : const Text('마지막 데이터입니다'),
-            );
-          }
-          final pItem = cp.data[index];
+      child: RefreshIndicator(
+        onRefresh: () async {
+          ref.read(widget.provider.notifier).paginate(
+            forceRefetch: true, //강제로 처음부터 데이터를 가져옴
+          );
+        },
+        child: ListView.separated(
+          physics: const AlwaysScrollableScrollPhysics(),
+          controller: controller,
+          itemCount: cp.data.length + 1,
+          itemBuilder: (_, index) {
+            if (index == cp.data.length) {
+              return Center(
+                child: cp is CursorPaginationfetchingMore
+                    ? const CircularProgressIndicator()
+                    : const Text('마지막 데이터입니다'),
+              );
+            }
+            final pItem = cp.data[index];
 
-         return widget.itemBuilder(
-           context,
-           index,
-           pItem,
-         );
-        },
-        separatorBuilder: (_, index) {
-          return const SizedBox(height: 16.0);
-        },
+           return widget.itemBuilder(
+             context,
+             index,
+             pItem,
+           );
+          },
+          separatorBuilder: (_, index) {
+            return const SizedBox(height: 16.0);
+          },
+        ),
       ),
     );
   }
